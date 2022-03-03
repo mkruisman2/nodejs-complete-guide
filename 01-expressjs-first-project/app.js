b07c6491817bfde479f88f8a2a813a9f346cd7d6
Marit Kruisman <maritkruisman@hotmail.com>
2022-03-03 16:29:08 -0500

    Adding complete expressjs first project with routing and html files

--- 01-expressjs-first-project/app.js
+++ 01-expressjs-first-project/app.js
@@ -1,19 +1,23 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use('/add-product', (req, res, next) => {
-    console.log("In product middleware!");
-    console.log(req.url);
-    res.send('<h1>The Add Product Page</h1>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    console.log("In another middleware!");
-    console.log(req.url);
-    res.send('<h1>Hello from Express</h1>');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
