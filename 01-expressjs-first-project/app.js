d4b289433bb7d7fce47d00a980d9d79402986037
Marit Kruisman <maritkruisman@hotmail.com>
2022-03-17 13:07:58 -0400

    Changed project to use ejs template engine

--- 01-expressjs-first-project/app.js
+++ 01-expressjs-first-project/app.js
@@ -2,12 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { engine } = require('express-handlebars');
+// const { engine } = require('express-handlebars');
 
 const app = express();
-app.engine('hbs', engine({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
-// app.set('view engine', 'pug'); // Set the engine which is used to render any dynamic templates
+app.set('view engine', 'ejs');
+// app.engine('hbs', engine({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs'); // Set the engine to express-handlebars which is used to render any dynamic templates
+// app.set('view engine', 'pug'); // Set the engine to pug which is used to render any dynamic templates
 app.set('views', 'views'); 
 // Set the directory where to find the views folder/files. Default is 'views' so no need to add this line if you don't have your files in a different folder.
 
@@ -23,7 +24,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: "" });
 });
 
 app.listen(3000);
