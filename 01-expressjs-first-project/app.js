f1081e83f70f9ec3be9e9c719cbd07a7017dca7e
Marit Kruisman <maritkruisman@hotmail.com>
2022-03-17 11:33:47 -0400

    Changed project to use Pug template engine

--- 01-expressjs-first-project/app.js
+++ 01-expressjs-first-project/app.js
@@ -4,8 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'pug'); // Set the engine which is used to render any dynamic templates
+app.set('views', 'views'); 
+// Set the directory where to find the views folder/files. Default is 'views' so no need to add this line if you don't have your files in a different folder.
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,11 +16,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- 01-expressjs-first-project/routes/shop.js
+++ 01-expressjs-first-project/routes/shop.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: 'My Shop', path: '/' }); // Will render the template engine file with engine that's set. Second argument is passing data into the template.
 });
 
 module.exports = router;
